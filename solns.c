2b42d3325f0dc7d1e05e0e3987dbd6e5a077c9a9
SrikarKorlam <84702997+SrikarKorlam@users.noreply.github.com>
2021-06-15 19:54:26 +0530

    Done!

--- solns.c
+++ solns.c
@@ -10,54 +10,6 @@ int factors(int, int []);
 int repeat(int [], int , int);
 int primetest(int);
 
-
-/*int main(void) {
-  int x1[] = {9,5,6,10,2,-3,4};
-  assert (max(x1, 7) == 10);
-
-  int y1[] = {5};
-  assert (max(y1, 1) == 5);
-  
-  
-  int x2[] = {9,5,6,10,2,-3,4};
-  assert (min(x2, 7) == -3);
-
-  int y2[] = {5};
-  assert (min(y2, 1) == 5);
-  
-  
-  int x3[] = {9,5,6,10,2,-3,4};
-  assert ((average(x3, 7) - 4.7142) < 0.001);
-
-  int y3[] = {5};
-  assert (average(y3, 1) == 5.0);
-  
-  
-  int x4[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
-assert ((mode(x4, 12) == 4));
-
-  int y4[] = {5};
-  assert (mode(y4, 1) == 5);
-  
-  
-  int ret[100] = {0};
-  int count = factors(180, ret);
-  assert (count == 5);
-  assert (ret[0] == 2);
-  assert (ret[1] == 2);
-  assert (ret[2] == 3);
-  assert (ret[3] == 3);
-  assert (ret[4] == 5);
-
-
-  count = factors(143, ret);
-  assert (count == 2);
-  assert (ret[0] == 11);
-  assert (ret[1] == 13);
-  
-}*/
-
-
 int max(int array[], int i){
 int m=array[0];
    for(int x=1;x<i;x++){
